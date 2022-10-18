52f8885135b906a052842058b25a2b58fd690ccd
BrazyWRLD <114942921+BrazyWRLD@users.noreply.github.com>
2022-10-18 22:36:21 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import random
 turns = ['rock', 'paper', 'scissors']
+human_turns = []
+computer_turns = []
+win_history = []
 
 while(True):
     human_turn = input("Enter your turn, or type exit: ")
@@ -9,14 +12,28 @@ while(True):
         print('Thank you for playing! Bye bye')
         break
 
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
         print('Draw!')
+        win_history.append('Draw')
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
+        win_history.append('Human wins')
     elif human_turn == 'paper' and computer_turn == 'rock':
         print('Human wins!')
+        win_history.append('Human wins')
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('Human wins!')
+        win_history.append('Human wins')
     else:
         print('Computer wins!')
+        win_history.append('Computer wins')
+
+
+print(f'You have played {len(human_turns)} times')
+print('Cilveka gajieni: ', human_turns)
+print('Datora gajieni: ', computer_turns)
+print('Uzvareja: ', win_history)
